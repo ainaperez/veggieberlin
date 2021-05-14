e12a61bd735dd3e4da4a4285634381e0d7d07552
Aina Perez <ainaperez@Ainas-MacBook-Pro.local>
2021-05-14 22:45:12 +0200

    arxius per a la segona pac

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // burger menu
 
-window.applyBurgerMenu = function applyBurgerMenu() {
+window.applyBurgerMenu = function applyBurgerMenu () {
   const x = document.getElementById('ul-nav')
   if (x.className === 'nav_bar') {
     x.className += ' nav_active'
@@ -12,23 +12,33 @@ window.applyBurgerMenu = function applyBurgerMenu() {
 // Category filters
 
 window.onload = function () {
-  document.getElementById('all')
   filterSelection('all')
-  var btnContainer = document.getElementById("btnId");
-  var btns = btnContainer.getElementsByClassName("btn");
-  for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function () {
-      var current = document.getElementsByClassName("active");
-      current[1].className = current[1].className.replace(" active", "");
-      this.className += " active";
+  function filterSelection (c) {
+    let i
+    const x = document.getElementsByClassName('content_filter')
+    if (c === 'all') c = ''
+    for (i = 0; i < x.length; i++) {
+      removeClass(x[i], 'show')
+      if (x[i].className.indexOf(c) > -1) addClass(x[i], 'show')
+    }
+  }
+  document.getElementById('all')
+
+  const btnContainer = document.getElementById('btnId')
+  const btns = btnContainer.getElementsByClassName('btn')
+  for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', function () {
+      const current = document.getElementsByClassName('active')
+      current[1].className = current[1].className.replace(' active', '')
+      this.className += ' active'
     })
   }
 }
 
 window.filterSelection = function filterSelection (c) {
-  var x, i
-  x = document.getElementsByClassName('content_filter')
-  if (c === 'all') c = '';
+  let i
+  const x = document.getElementsByClassName('content_filter')
+  if (c === 'all') c = ''
   for (i = 0; i < x.length; i++) {
     removeClass(x[i], 'show')
     if (x[i].className.indexOf(c) > -1) addClass(x[i], 'show')
@@ -36,9 +46,9 @@ window.filterSelection = function filterSelection (c) {
 }
 
 function addClass (element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split('')
-  arr2 = name.split(' ');
+  let i
+  const arr1 = element.className.split('')
+  const arr2 = name.split(' ')
   for (i = 0; i < arr2.length; i++) {
     if (arr1.indexOf(arr2[i]) === -1) {
       element.className += ' ' + arr2[i]
@@ -47,13 +57,32 @@ function addClass (element, name) {
 }
 
 function removeClass (element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(' ');
-  arr2 = name.split(' ');
+  let i
+  const arr1 = element.className.split(' ')
+  const arr2 = name.split(' ')
   for (i = 0; i < arr2.length; i++) {
     while (arr1.indexOf(arr2[i]) > -1) {
       arr1.splice(arr1.indexOf(arr2[i]), 1)
     }
   }
   element.className = arr1.join(' ')
+}
+
+// aricle images zoom
+
+window.onload = function () {
+  const modal = document.getElementById('myModal')
+
+  const img = document.getElementById('myImg')
+  const modalImg = document.getElementById('img01')
+
+  img.onclick = function () {
+    modal.style.display = 'block'
+    modalImg.src = this.src
+  }
+  const span = document.getElementsByClassName('close_modal')[0]
+
+  span.onclick = function () {
+    modal.style.display = 'none'
+  }
 }
